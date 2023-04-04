3f09a7dacbab9fde56e2b1eda9d23844bbf526df
Kirill Kozlov <kozlov.kirill.v.work@gmail.com>
2023-04-04 13:37:33 +0700

    Implement read/write between external 32-bit reg. and I/O regs. 0xf6-0xf9

--- asm/main.asm
+++ asm/main.asm
@@ -19,8 +19,8 @@ rowController:
 asect 0xf6
 fristByte:
 
-asect 4
-bytesInRow:
+asect 0xf9
+lastByte:
 
 #===============================
   ### Place for subroutines ###
@@ -33,7 +33,22 @@ asect 0x00
 start:
 	addsp -16  # Move SP before I/O addresses
 
-	# place your main code here
+	# ====
+	# Test code for row I/O system
+	ldi r0, rowController
+	ld r0, r1
+	ldi r0, fristByte
+	ldi r1, 0
+	ldi r2, lastByte
+	while
+		cmp r0, r2
+	stays le
+		ld r0, r3
+		st r1, r3
+		inc r1
+		inc r0		
+	wend
+	# ====
 
 	halt
 end
