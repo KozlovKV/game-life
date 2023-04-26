89481615213da997482394810890369e0f3cd491
Kirill Kozlov <kozlov.kirill.v.work@gmail.com>
2023-04-26 13:22:55 +0700

    Move to main file

--- asm/processBitInByte.asm
+++ asm/processBitInByte.asm
@@ -6,18 +6,16 @@ processBitInByte:
 		tst r0
 	is eq
 		ldi r0, birthConditions
-		ld r0, r0
 	else
 		ldi r0, deathConditions
-		ld r0, r0
 	fi
+	ld r0, r0
 	move r2, r1
 	dec r1
 	jsr getBit
 	move r0, r2
 	pop r1
 	pop r0
-	ldi r1, 1
 	if
 		tst r2
 	is nz
