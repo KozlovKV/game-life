0bf14e9a91f3ffc00fbf3eed82745e2ca1e968de
Kirill Kozlov <kozlov.kirill.v.work@gmail.com>
2023-04-26 13:17:55 +0700

    Merge bit_getter_inverter

--- asm/main.asm
+++ asm/main.asm
@@ -103,7 +103,28 @@ asect 0x100
 #==============================#
 #     Place for subroutines    #
 #==============================#
+getBit:
+	while
+		tst r1
+	stays nz
+		shr r0
+		dec r1
+	wend
+	ldi r1, 1
+	and r1, r0
+rts
 
+invertBit:
+	ldi r2, 1
+	while
+		tst r1
+	stays nz
+		shl r2
+		dec r1
+	wend
+	xor r2, r0
+rts
+		
 #===============================
 
 start:
